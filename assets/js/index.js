20ca6d3aaa1cb911973cc784d37ba37c92b4f732
Esther Bacelar <esther.v.bacelar@gmail.com>
2021-10-10 05:05:28 -0300

    Organizando js parte do lucro e prejuizo.

--- assets/js/index.js
+++ assets/js/index.js
@@ -22,23 +22,24 @@
         inicio()
     }
     
-    const adicionarTransacoesReais = ({ valor, nome, id, opcao }) => {
+    const adicionarTransacoesReais = ({ valor, nome, id }) => {
         const operador = valor < 0 ? '-' : '+'
         const cssClass = valor < 0 ? 'sinalmenos' : 'sinalmais'
-        const transacoesDeMercadorias = valor < 0 ? '#compra' : '#venda'
-        const valorSemOperador = Math.abs(valor)
+        const valorSemOperador = Math.abs(valor).toLocaleString('pt-BR', {
+            currency: 'BRL',
+            style: 'currency',
+            minimumFractionDigits: 2
+          });
         const li = document.createElement('li')
-
         li.classList.add(cssClass)
         li.innerHTML = `
         <hr class="hrspan"></hr>
-        <span class="operadores">${operador}</span>
+        <span class="operadores" id="operadores">${operador}</span>
         <span class="linha1-texto-merc" id="mercad1">${nome}</span>
-        <span class="linha1-texto-val" id="valor1"> R$ ${valorSemOperador}</span>
+        <span class="linha1-texto-val" id="valor1"> ${valorSemOperador}</span>
         `
         linha1Ul.append(li)
-
-    }
+    }  
 
     const pegueCompras = valoresDeTransacoes => Math.abs(valoresDeTransacoes
         .filter(valor => valor < 0)
@@ -52,17 +53,22 @@
 
     const pegueTotal = valoresDeTransacoes => valoresDeTransacoes
         .reduce((acumulador, transacao) => acumulador + transacao, 0)
-        .toFixed(2)
+        .toLocaleString('pt-BR', {
+            currency: 'BRL',
+            style: 'currency',
+            minimumFractionDigits: 2});
 
     const atualizarValoresTotais = () => {
         const valoresDeTransacoes = transacoes.map(({ valor }) => valor)
         const total = pegueTotal(valoresDeTransacoes)
+    /*  Se precisar jogar valores de Compras e Vendas
         const vendas  = pegueVendas(valoresDeTransacoes)
-        const compras = pegueCompras(valoresDeTransacoes)
+        const compras = pegueCompras(valoresDeTransacoes) 
+        */
         
-        totalAtualizado.textContent = `R$ ${total}`
+        totalAtualizado.textContent = `${total}`
 
-        if (total > 0) {
+        if (totalAtualizado > 0) {
             lucroPrejuizo.textContent = `[LUCRO]` 
         } else {
             lucroPrejuizo.textContent = `[PREJUÍZO]`  
@@ -91,7 +97,8 @@
             valor: Number(transacaoValorMercadoria) 
         })
     }
-
+    const transacaoNomeMercadoria = nomeMercadoriaInput.value.trim()
+    const transacaoValorMercadoria = valorInput.value.trim()
     const limparInputs = () => {
         transacaoNomeMercadoria.value =  ''
         transacaoValorMercadoria.value =  ''
@@ -105,7 +112,10 @@
         const transacaoOpcoes = opcaoVenda.value.trim() || opcaoCompra.value.trim() 
         const transacaoNomeMercadoria = nomeMercadoriaInput.value.trim()
         const transacaoValorMercadoria = valorInput.value.trim()
-        const algumInputVazio = transacaoOpcaoVenda === '' || transacaoOpcaoCompra === '' || transacaoNomeMercadoria === '' || transacaoValorMercadoria === ''
+        const algumInputVazio = transacaoOpcaoVenda === '' || 
+        transacaoOpcaoCompra === '' || 
+        transacaoNomeMercadoria === '' || 
+        transacaoValorMercadoria === ''
 
         if (algumInputVazio) {
             alert('Por favor preencha todos os campos abaixo')
@@ -119,3 +129,74 @@
     }
 
     formulario.addEventListener('submit', lidarComFormEnviar)
+
+    /* Fazer divs de Nenhuma Transação Cadastrada e LucroPrejuízo aparecerem e desaparecerem 
+    nos momentos corretos */
+
+    const botao = document.querySelector('#enviar');
+    const divNadaCadastrado = document.querySelector('#semtrans');
+    const divLucroPrejuizo = document.querySelector('#lucro');
+
+    botao.onclick = function() {
+        divNadaCadastrado.setAttribute('class', 'esconder');
+        divLucroPrejuizo.setAttribute('class', 'aparecer');
+    }
+
+      const divOperadores = document.querySelector('#operadores');
+      const divMercadoria = document.querySelector('#mercad1');
+      const divValor = document.querySelector('#valor1');
+
+      if (divOperadores && divMercadoria && divValor !== '' ) {
+        document.getElementById("semtrans").style.display = "none";
+        document.getElementById("lucro").style.display = "block";
+      }
+
+    /* Ao clicar no menu 'Limpar Dados' todos os dados do extrato são apagados */
+
+    function acaoLimpar() {
+        decisao = confirm('Deseja realmente limpar os dados?')
+    
+        if (decisao) {
+            localStorage.clear();
+            limparAutoComplete();
+        }
+        else
+            return false;
+    }
+
+    function testaCampoValor(e) {
+        e.preventDefault()
+    
+        if ((/[0-9]/g).test(e.key)) {
+            e.target.value += e.key
+        }
+    } 
+
+    function formatarMoeda() {
+        let elemento = document.getElementById('valor');
+        let valor = elemento.value;
+
+        valor = valor + '';
+        valor = parseFloat(valor.replace(/[\D]+/g, ''));
+        valor = valor + '';
+        valor = valor.replace(/([0-9]{2})$/g, ".$1");
+
+        if (valor.length > 6) {
+            valor = valor.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
+        }
+
+        let escolhendoVenda = document.querySelector('#venda').text;
+        let selected_options = document.querySelector('#trans').selectedOptions;
+    
+        for(i=0; i<selected_options.length; i++)
+        if (selected_options[i].text !== escolhendoVenda) {
+            elemento.value = '-' + valor;
+            console.log('compra -')
+        } else {
+            elemento.value = valor;
+            console.log('venda +')
+        }
+        if (valor == 'NaN') {
+            elemento.value = '';
+        }
+    }
